8c1f3f217e1ac947a984a05438ff6c1387d29b42
Paulo <paulotec96@gmail.com>
2019-03-24 23:32:53 -0300

    Adding details to Salmão

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,12 +1,20 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*'),
-        new Recipe('B Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*')
+        new Recipe('Bacalhau', 'Esse é um bacalhau muito gostoso', 'https://www.teleculinaria.pt/wp-content/uploads/2016/09/Bacalhau-com-natas-e-delicias-do-mar-6_V2.jpg',[
+            new Ingredient('Bacalhau', 1),
+            new Ingredient('Leite', 2)
+        ]),
+        new Recipe('Salmão Defumado', 'Esse é um delicioso salmão defumado', 'https://www.gazetadopovo.com.br/blogs/panela-do-anacreon/wp-content/uploads/sites/69/import/Salmao_defumado_com_risoto_de_alho_poro.JPG',
+        [
+            new Ingredient('Salmão', 1),
+            new Ingredient('Leite', 2)
+        ])
       ];
 
     getRecipes() {
