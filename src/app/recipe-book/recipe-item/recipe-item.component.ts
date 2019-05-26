72aba71800d38660c956df42e76fd559c7ed8ab8
Paulo <paulotec96@gmail.com>
2019-05-26 18:47:39 -0300

    Configuring Route Parameters

--- src/app/recipe-book/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,16 +8,10 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeItem: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
 
-  }
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipeItem);
   }
 
 }
